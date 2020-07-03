30a2c3dc5d56b8acb91158fb759c5a0901ef092d
annia valdes <millerl.mb79@gmail.com>
2020-07-02 21:09:46 -0500

    update

--- annia/assets/js/script.js
+++ annia/assets/js/script.js
@@ -1,1 +1,25 @@
 var dating = '';
+
+
+var submitBtn = document.querySelector('#submit');
+
+submitBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    var city = $("input").val().trim();
+    console.log(city)
+    showEvent(city)
+})
+
+function showEvent(cityName) {
+ 
+       var apikey="4rwvR5WRLvh2Sb5c";
+  
+    //call the API request
+    fetch(`https://api.eventful.com/json/events/search?app_key=${apikey}&keywords=music&location=${cityName}&date=Today`)     
+        .then(function(response) {
+            console.log("este", response)                             
+           
+        })
+
+}
+//showEvent(houston);
