d59e543ff1b72b69c5c8da39b210231642e1d03a
Victor Cesar Lopez <civ187@gmail.com>
2020-07-02 20:47:00 -0500

    added to Victor folder

--- victor/assets/js/script.js
+++ victor/assets/js/script.js
@@ -1,1 +1,33 @@
-var dating = '';
+var inputEl = document.getElementById("city");
+var searchEl = document.getElementById("search");
+
+//start of Victor's script
+var citySubmitHandler = function(event) {
+    event.preventDefault();
+    var cityName = inputEl.value.trim();
+    console.log(cityName)
+
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(function(response) {
+        response.json().then(function(data) {
+            var drinkName = data.drinks[0].strDrink;
+            var drinkImg = data.drinks[0].strDrinkThumb;
+            var drinkGlass = data.drinks[0].strGlass;
+            var drinkInstructions = data.drinks[0].strInstructions;
+            var drinkIngredient = data.drinks[0].strIngredient1;
+
+            console.log(drinkName);
+            console.log(drinkImg);
+            console.log(drinkGlass);
+            console.log(drinkInstructions);
+            console.log(drinkIngredient);
+
+        })
+
+    })
+};
+//end of Victor's script
+
+
+
+searchEl.addEventListener("click", citySubmitHandler);
+
