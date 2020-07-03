a57138cb7f6a3d68c509e672ba6c8e8e0b703e1f
annia valdes <millerl.mb79@gmail.com>
2020-07-02 21:55:53 -0500

    made request to API

--- annia/assets/js/script.js
+++ annia/assets/js/script.js
@@ -1,13 +1,23 @@
-var dating = '';
+var cityFormEl = document.getElementById("city-form")
+var textInputEl = document.getElementById("text-input");
+var stateSelectEl = document.getElementById("state-select");
+var submitButtonEl = document.getElementById("submit-button");
+var deleteButtonEl = document.getElementById("delete-button");
+var victorEl = document.getElementById("victor");
+var anniaEl = document.getElementById("annia");
+var shaneEl = document.getElementById("shane");
+var tylerEl = document.getElementById("tyler");
+var joshuaEl = document.getElementById("joshua");
+var inputCity = "";
+var inputState = "";
 
-
-var submitBtn = document.querySelector('#submit');
-
-submitBtn.addEventListener('click', function (event) {
+submitButtonEl.addEventListener("click", function(event){
     event.preventDefault();
-    var city = $("input").val().trim();
-    console.log(city)
-    showEvent(city)
+    inputCity = document.querySelector("input[name='city']").value.trim().toLowerCase();
+    cityFormEl.reset();
+    console.log(inputCity);
+    showEvent(inputCity);
+
 })
 
 function showEvent(cityName) {
@@ -17,9 +27,11 @@ function showEvent(cityName) {
     //call the API request
     fetch(`https://api.eventful.com/json/events/search?app_key=${apikey}&keywords=music&location=${cityName}&date=Today`)     
         .then(function(response) {
-            console.log("este", response)                             
-           
+            return response.json();                           
+        })
+        .then(function(data) {
+            console.log(data)
         })
 
 }
-//showEvent(houston);
+
