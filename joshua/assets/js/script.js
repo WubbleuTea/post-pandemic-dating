de749016c48e4de5f6fa95a8271511ba37c46f5f
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-04 20:15:46 -0500

    style update

--- joshua/assets/js/script.js
+++ joshua/assets/js/script.js
@@ -10,6 +10,7 @@ var shaneEl = document.getElementById("shane");
 var tylerEl = document.getElementById("tyler");
 var joshuaEl = document.getElementById("joshua");
 var weatherRowEl = document.getElementById("weather-row")
+var formErrorsEl = document.getElementById("form-errors")
 var inputCity = "";
 var inputState = "";
 var searchedCity = [];
@@ -18,6 +19,7 @@ var checkStorageCity = JSON.parse(localStorage.getItem("city"))
 var checkStorageState = JSON.parse(localStorage.getItem("state"))
 
 var createCity = function(){
+    formErrorsEl.innerHTML = ""
     var buttonName = inputCity.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) + "," + inputState
     var cityButton = document.createElement("button")
         cityButton.className = "btn"
@@ -27,6 +29,7 @@ var createCity = function(){
 
     cityButton.addEventListener("click", function(event){
         event.preventDefault();
+        formErrorsEl.innerHTML = ""
         weatherRowEl.innerHTML = "";
         placeArr = event.target.textContent.split(",")
         inputCity = placeArr[0]
@@ -37,19 +40,24 @@ var createCity = function(){
 var getWeather = function(weather){
     fetch("https://api.weatherapi.com/v1/forecast.json?key=898f900d29334755948192951200207&days=3&hour=19&q=" + inputCity + "," + inputState).then(function(response){
         if (response.ok) {
+            formErrorsEl.innerHTML = ""
+            response.json().then(function(data) {
             // if it is a new item push into arrays and local storage and create button
-            if (weather==true && !searchedCity.includes(inputCity)){
+            if (weather==true && !searchedCity.includes(inputCity) && data.location.country == "United States of America"){
                 searchedCity.push(inputCity)
                 localStorage.setItem("city", JSON.stringify(searchedCity))
                 searchedState.push(inputState);
                 localStorage.setItem("state", JSON.stringify(searchedState))
                 createCity();
-            }
+            } 
             //if button is already made this will run
-            response.json().then(function(data) {
-                //if not useable
+           
+            //if not useable
             if (data.location.country != "United States of America") {
-                alert("not a city in the USA")
+                var errorOne = document.createElement("div")
+                errorOne.className = "col12 red accent-4"
+                errorOne.innerHTML = "<h2>City is not in the USA</h2>"
+            formErrorsEl.appendChild(errorOne);
                 inputCity = ""
                 inputState = ""
                 cityFormEl.reset();
@@ -64,8 +72,8 @@ var getWeather = function(weather){
 
                     weatherForecast.className = "col s12 m4";
                     weatherForecast.innerHTML =  "<div class='card blue-grey darken-1'>"+
-                    "<div class='card-content white-text'>" +
-                        "<span class='card-title'>"+ forecastDate + "</span>" + "<img src='" + getForecast.day.condition.icon + "'/>" +
+                        "<div class='card-content white-text'>" +
+                        "<span class='card-title'>"+ forecastDate + "</span>" + "<div><img src='//cdn.weatherapi.com/weather/64x64/day/" + getForecast.day.condition.icon.slice(-7) + "'/></div>" +
                         "<p>High: "+ getForecast.day.maxtemp_f + "°F</p>"  +
                         "<p>Low: " + getForecast.day.mintemp_f + "°F</p>" + 
                         "<p>Rain: " + getForecast.day.daily_chance_of_rain + "%</p>"
@@ -76,9 +84,13 @@ var getWeather = function(weather){
                 console.log(data.forecast.forecastday[0].day.avgtemp_f)
             }
             })  
+
             // if recieved a error     
         } else {
-            console.log("This did not work")
+            var errorOne = document.createElement("div")
+                errorOne.className = "col12 red accent-4"
+                errorOne.textContent = "Something did not work. Try again."
+            formErrorsEl.appendChild(errorOne);
             inputCity = ""
             inputState = ""
             cityFormEl.reset();
