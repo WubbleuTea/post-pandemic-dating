acfe9f38e3490e5d30e9ca5ce73994a7310027f4
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-05 20:04:29 -0500

    update to page styling and functions

--- joshua/assets/js/script.js
+++ joshua/assets/js/script.js
@@ -6,11 +6,12 @@ var deleteButtonEl = document.getElementById("delete-button");
 var pastCitiesEl = document.getElementById("past-cities")
 var victorEl = document.getElementById("victor");
 var anniaEl = document.getElementById("annia");
-var shaneEl = document.getElementById("shane");
+var holidayInfoEl = document.getElementById("holiday-info");
 var tylerEl = document.getElementById("tyler");
 var joshuaEl = document.getElementById("joshua");
 var weatherRowEl = document.getElementById("weather-row")
 var formErrorsEl = document.getElementById("form-errors")
+var currentCityEl = document.getElementById("current-city")
 var inputCity = "";
 var inputState = "";
 var searchedCity = [];
@@ -22,7 +23,7 @@ var createCity = function(){
     formErrorsEl.innerHTML = ""
     var buttonName = inputCity.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) + "," + inputState
     var cityButton = document.createElement("button")
-        cityButton.className = "btn"
+        cityButton.className = "btn pink blue-grey"
         cityButton.textContent = buttonName
     pastCitiesEl.appendChild(cityButton);
     weatherRowEl.innerHTML = ""
@@ -55,7 +56,7 @@ var getWeather = function(weather){
             //if not useable
             if (data.location.country != "United States of America") {
                 var errorOne = document.createElement("div")
-                errorOne.className = "col12 red accent-4"
+                errorOne.className = "card col 12 red accent-4"
                 errorOne.innerHTML = "<h2>City is not in the USA</h2>"
             formErrorsEl.appendChild(errorOne);
                 inputCity = ""
@@ -63,6 +64,12 @@ var getWeather = function(weather){
                 cityFormEl.reset();
                 return
             }
+            currentCityEl.innerHTML = "";
+            var currentCityTitle = document.createElement("div")
+                currentCityTitle.className = ""
+                currentCityTitle.innerHTML = "<h2 class='col s12 card-panel pink lighten-1 center-align city-name'>" + inputCity.replace(/(^\w|\s\w)/g, m => m.toUpperCase()) + "," + inputState; + "</h2>"
+                currentCityEl.appendChild(currentCityTitle);
+           document.getElementById("weather").className = "row show"; 
             //else run the weather forcast
             for (var i=0; i< 3; i++) {
                 var getForecast = data.forecast.forecastday[i]
@@ -71,17 +78,18 @@ var getWeather = function(weather){
                 var weatherForecast = document.createElement("div")
 
                     weatherForecast.className = "col s12 m4";
-                    weatherForecast.innerHTML =  "<div class='card blue-grey darken-1'>"+
+                    weatherForecast.innerHTML =  "<div class='card blue-grey darken-1 z-depth-5'>"+
                         "<div class='card-content white-text'>" +
                         "<span class='card-title'>"+ forecastDate + "</span>" + "<div><img src='//cdn.weatherapi.com/weather/64x64/day/" + getForecast.day.condition.icon.slice(-7) + "'/></div>" +
                         "<p>High: "+ getForecast.day.maxtemp_f + "°F</p>"  +
                         "<p>Low: " + getForecast.day.mintemp_f + "°F</p>" + 
-                        "<p>Rain: " + getForecast.day.daily_chance_of_rain + "%</p>"
+                        "<p>Rain: " + getForecast.day.daily_chance_of_rain + "%</p>" +
                     "</div>" +
                     "</div>"
                 weatherRowEl.appendChild(weatherForecast)
                 console.log(data)
                 console.log(data.forecast.forecastday[0].day.avgtemp_f)
+                
             }
             })  
 
@@ -96,8 +104,10 @@ var getWeather = function(weather){
             cityFormEl.reset();
             return
         }
+        
     })
 }
+
 
 submitButtonEl.addEventListener("click", function(event){
     if (stateSelectEl.value == 0) {
@@ -105,10 +115,9 @@ submitButtonEl.addEventListener("click", function(event){
         formEl.reset();
     }
     event.preventDefault();
-    inputCity = document.querySelector("input[name='city']").value.trim().replace(/(^\w|\s\w)/g, m => m.toUpperCase())
+    inputCity = document.querySelector("input[name='city']").value.trim().toLowerCase();
     inputState = stateSelectEl.value
     cityFormEl.reset();
-    console.log(inputCity, inputState);
     getWeather(true);
 
 })
