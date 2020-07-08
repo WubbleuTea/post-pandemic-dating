3d7f36021d88ad08a2b60021007ace6765e0edc3
Victor Cesar Lopez <civ187@gmail.com>
2020-07-08 16:04:09 -0500

    Commit on 7/8 @ 4:00pm, needs intergration into master

--- victor/assets/js/script.js
+++ victor/assets/js/script.js
@@ -13,74 +13,55 @@ var differentDrinkEl = document.getElementById("drink-button");
 
 //start of Victor's script
 var drinkHandler = function(event) {
-    // event.preventDefault();               
-    // var cityName = inputEl.value.trim();
-    // console.log(cityName)
-    //  document.getElementById("cocktail").className = "row show";
+
+    //   document.getElementById("cocktail").className = "row show";
      
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+
             var drinkIngredientEl = document.getElementById("drinkIngredients");
-            
+            // clears html so that ghost images do not appear
+            drinkNameEl.innerHTML = "";
             drinkIngredientEl.innerHTML="";
+            drinkGlassEl.innerHTML = "";
+            drinkPicEl.setAttribute("src","");
+            drinkInsEl.innerHTML = "";
             var drinkType = data.drinks[0].strAlcoholic;
-            console.log(drinkType);
 
             // this checks to make sure the drink is alcoholic
             if (drinkType == "Non alcoholic" || drinkType == "Optional alcohol") {
                 drinkHandler(event);
              };
-            // information form the API is being added to the index.html 
-            var drinkName = data.drinks[0].strDrink;
-            // console.log(drinkName);
-            drinkNameEl.innerHTML = drinkName;
-            
-            var drinkImg = data.drinks[0].strDrinkThumb;
-            // console.log(drinkImg);
-            drinkPicEl.setAttribute("src",drinkImg);
 
+            // information from the API is being added to the index.html 
+            var drinkName = data.drinks[0].strDrink;
+            drinkNameEl.innerHTML = drinkName;
+            var drinkImg = data.drinks[0].strDrinkThumb;
+            drinkPicEl.setAttribute("src",drinkImg);
             var drinkGlass = data.drinks[0].strGlass;
-            // console.log(drinkGlass);
             drinkGlassEl.innerHTML = "Served in a: " + drinkGlass;
-            
             var drinkInstructions = data.drinks[0].strInstructions;
-            // console.log(drinkInstructions);
             drinkInsEl.innerHTML = drinkInstructions;
 
-            var i=1;
-            
             // object bracket notation
+            // While statement is used to read indredients and end when it hits a null value
+            var i=1;
              while (data.drinks[0]["strIngredient"+i]) {
-                    console.log(data.drinks[0]["strMeasure"+i],data.drinks[0]["strIngredient"+i]);
-                    //var drinkItem = data.drinks[0]["strMeasure"+i] + " - " + data.drinks[0]["strIngredient"+i];
                     var drinkMeasure = data.drinks[0]["strMeasure"+i];
-                    //var drinkItem01 = data.drinks[0]["strIngredient"+i];
-                    console.log(drinkMeasure);
-                    // var drinkItme = "";
-                   // console.log("drink Item if there is no measurement" ,drinkItem01);
-                    //add if statement to check for null indredient values
                     if (data.drinks[0]["strMeasure"+i] == null ) {
                         drinkItem = data.drinks[0]["strIngredient"+i];
                     } else {
                         drinkItem = data.drinks[0]["strMeasure"+i] + " - " + data.drinks[0]["strIngredient"+i];
                     }
-                    
-                    console.log("drink Item is" ,drinkItem);
-                     
                     var drinkItemEl = document.createElement("p");
                     drinkItemEl.innerHTML = drinkItem;
                     drinkIngredientEl.appendChild(drinkItemEl);
-
                     i++;
                 };
         })
     })
 };
 //end of Victor's script
-
-
-
 inputEl.addEventListener("click", drinkHandler);//this initiates the drink function
 differentDrinkEl.addEventListener("click", drinkHandler);//this initiates the drink function without resetting the page
 
