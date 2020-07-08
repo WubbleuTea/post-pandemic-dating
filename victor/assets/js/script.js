e24ff54ad1dd0b567057000dced94421a607f903
Victor Cesar Lopez <civ187@gmail.com>
2020-07-08 12:02:11 -0500

    not finalized

--- victor/assets/js/script.js
+++ victor/assets/js/script.js
@@ -8,19 +8,20 @@ var drinkNameEl = document.getElementById("drinkName");
 var drinkGlassEl = document.getElementById("drinkGlass");
 var drinkInsEl = document.getElementById("drinkIns");
 var differentDrinkEl = document.getElementById("drink-button");
-var drinkIngredientEl = document.getElementById("drinkIngredients");
-// var drinkIngredientEl = document.querySelectorAll(".drinkIngredients");
+
 //End of variables that are part of Victor's script
 
 //start of Victor's script
 var drinkHandler = function(event) {
-    event.preventDefault();               
+    // event.preventDefault();               
     // var cityName = inputEl.value.trim();
     // console.log(cityName)
 
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(function(response) {
         response.json().then(function(data) {
             console.log(data);
+            var drinkIngredientEl = document.getElementById("drinkIngredients");
+            drinkIngredientEl.innerHTML="";
             var drinkType = data.drinks[0].strAlcoholic;
             console.log(drinkType);
 
@@ -36,10 +37,10 @@ var drinkHandler = function(event) {
             var drinkImg = data.drinks[0].strDrinkThumb;
             // console.log(drinkImg);
             drinkPicEl.setAttribute("src",drinkImg);
-            
+
             var drinkGlass = data.drinks[0].strGlass;
             // console.log(drinkGlass);
-            drinkGlassEl.innerHTML = "The "+ drinkName + " is served in a " + drinkGlass+".";
+            drinkGlassEl.innerHTML = "Served in a: " + drinkGlass;
             
             var drinkInstructions = data.drinks[0].strInstructions;
             // console.log(drinkInstructions);
@@ -50,47 +51,34 @@ var drinkHandler = function(event) {
             // object bracket notation
              while (data.drinks[0]["strIngredient"+i]) {
                     console.log(data.drinks[0]["strMeasure"+i],data.drinks[0]["strIngredient"+i]);
-                    var drinkItem = data.drinks[0]["strMeasure"+i] + " - " + data.drinks[0]["strIngredient"+i];
-                   console.log("drink Item is" ,drinkItem);
-                //    drinkIngredientEl.createElement("p")
-                //    var drinkItemEl = document.createElement("p");
-                //    var elementId = "id"[i];
-                //    console.log(elementId);
-                   
-                //    drinkItemEl.setAttribute("id",i)
-                //     drinkItemEl.innerHTML = drinkItem;
-                   
-                   // var addDrinkItem = function( id="drinkIngredients", data) {
-                       
-                    //     }
-                        
-                    //     
-                    //     drinkItemEl.innerHTML = drinkItem;
-                        
-                    //     drinkIngredientEl.innerHTML = drinkItem;
+                    //var drinkItem = data.drinks[0]["strMeasure"+i] + " - " + data.drinks[0]["strIngredient"+i];
+                    var drinkMeasure = data.drinks[0]["strMeasure"+i];
+                    //var drinkItem01 = data.drinks[0]["strIngredient"+i];
+                    console.log(drinkMeasure);
+                    // var drinkItme = "";
+                   // console.log("drink Item if there is no measurement" ,drinkItem01);
+                    //add if statement to check for null indredient values
+                    if (data.drinks[0]["strMeasure"+i] == null ) {
+                        drinkItem = data.drinks[0]["strIngredient"+i];
+                    } else {
+                        drinkItem = data.drinks[0]["strMeasure"+i] + " - " + data.drinks[0]["strIngredient"+i];
+                    }
                     
-                    // addDrinkItem("drinkIngredients", drinkItem);
+                    console.log("drink Item is" ,drinkItem);
+                     
+                    var drinkItemEl = document.createElement("p");
+                    drinkItemEl.innerHTML = drinkItem;
+                    drinkIngredientEl.appendChild(drinkItemEl);
+
                     i++;
-                    
-             };
-
-             
-            
-            
-
-
-            
-
-
-
+                };
         })
-
     })
 };
 //end of Victor's script
 
 
 
-inputEl.addEventListener("click", drinkHandler);//this initiates the drink
+inputEl.addEventListener("click", drinkHandler);//this initiates the drink function
 differentDrinkEl.addEventListener("click", drinkHandler);
 
