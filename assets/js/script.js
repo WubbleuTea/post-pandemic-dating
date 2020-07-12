f633ce8ae1a9397eefa21b054286deca96260179
shanemcf <shanemcfadden95@yahoo.com>
2020-07-12 13:37:29 -0500

    deleted unneeded folder and updated comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,18 +134,18 @@ function showHoliday() {
     var todayDate = moment().format('MM/DD/YYYY')
     var tomorrowDate = moment().add(1, 'days').format('MM/DD/YYYY')
     var followingDate = moment().add(2, 'days').format('MM/DD/YYYY')
-    // connects dates to HTML and styles them
+    // connects dates to HTML and styles them with the current and following two days
     var blueHoliday0El = document.getElementById("blue-holiday0")
         blueHoliday0El.innerHTML = ""
         blueHoliday0El.className = "row show";
-        blueHoliday0El.innerHTML += "<span class='card-title'>" + todayDate + "</span>";
+        blueHoliday0El.innerHTML = "<span class='card-title'>" + todayDate + "</span>";
     var blueHoliday1El = document.getElementById("blue-holiday1")
         blueHoliday1El.innerHTML = ""
         blueHoliday1El.className = "row show";
-        blueHoliday1El.innerHTML += "<span class='card-title'>" + tomorrowDate + "</span>";
+        blueHoliday1El.innerHTML = "<span class='card-title'>" + tomorrowDate + "</span>";
     var blueHoliday2El = document.getElementById("blue-holiday2")
         blueHoliday2El.innerHTML = ""
-        blueHoliday2El.innerHTML += "<span class='card-title'>" + followingDate + "</span>";
+        blueHoliday2El.innerHTML = "<span class='card-title'>" + followingDate + "</span>";
         blueHoliday2El.className = "row show";
 
     // Key for the API
@@ -161,13 +161,14 @@ function showHoliday() {
                     holidayText.classList = "black-text";
                     holidayText.innerText = ""
 
-                    //runs if statement to display if there is a holiday
+                    //runs if statement to display if there is a holiday and displays it as a string
                     if (holiday.response.holidays.length == 0) {
                         holidayText.innerHTML = 'There are no holidays today.';
                         //runs else if statement for if there is a single holiday
                     } else if (holiday.response.holidays.length == 1) {
                         holidayText.innerText = "The holiday for this day is " + holiday.response.holidays[0].name + ".";
                         //runs else if statement for if there is more than one holiday
+                        // had difficulty adding in the data for multiple holidays so displays others as a string
                     } else if (holiday.response.holidays.length > 1) {
                         holidayText.innerText = "The holiday for this day is " + holiday.response.holidays[0].name + ' & there are other holidays.';
                         // runs an error statement if not working properly
@@ -185,7 +186,7 @@ function showHoliday() {
                         });
                         return
                     };
-                    // runs an if statement that appends the proper data to the proper date div
+                    // runs an if statement that appends the proper data to the proper date div with the holiday under the current date
                     if (i === 0) {
                         blueHoliday0El.appendChild(holidayText);
                     } else if (i === 1) {
